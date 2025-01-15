d6266e2c12dad27951abf495268716cc0e8e325c
sergio-contente <sergio.mcontente@gmail.com>
2025-01-15 18:23:23 +0100

    ✨ Tokenization and Position embedding

--- tokenizer.py
+++ tokenizer.py
@@ -46,10 +46,32 @@ class SimpleTokenizerV2:
 
 tokenizer = tiktoken.get_encoding("gpt2")
 text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
+    "Akwirw ier"
 )
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
 print(integers)
 strings = tokenizer.decode(integers)
 print(strings)
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+
+enc_sample = enc_text[50:]
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+print(f"x: {x}")
+print(f"y:      {y}")
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(context, "---->", desired)
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
